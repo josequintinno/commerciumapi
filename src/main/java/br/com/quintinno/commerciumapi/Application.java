6262d9d3d97c9e2a3fe0b1965083506d8fc5d061
José Quintinno <josequintinno@icloud.com>
2024-11-05 15:58:03 -0300

    🔵 feature: Configuração do arquivo POM.XML

--- src/main/java/br/com/quintinno/commerciumapi/Application.java
+++ src/main/java/br/com/quintinno/commerciumapi/Application.java
@@ -2,12 +2,23 @@ package br.com.quintinno.commerciumapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping
+	public String api() {
+		return "[API] Commercium: v1.0.0 (Port: 8080)";
 	}
 
 }
--- src/test/java/br/com/quintinno/commerciumapi/ApplicationTests.java
+++ src/test/java/br/com/quintinno/commerciumapi/ApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.quintinno.commerciumapi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
